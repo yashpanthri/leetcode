b2794ec7782542e3a877afedd36a2f9d684937c3
YASH PANTHRI <73519446+yashpanthri@users.noreply.github.com>
2026-01-08 23:57:33 -0500

    Added Bubblesort code

---
+++ bubble_sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n; i++) {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+}
+
+int main() {
+    vector<int> arr = {64, 34, 25, 12, 22, 11, 90};
+    bubbleSort(arr);
+
+    for (int x : arr)
+        cout << x << " ";
+    return 0;
+}
+
+/**Time Complexity:
+
+Worst/Average: O(n²)
+
+Best (already sorted): O(n)
+
+Space Complexity: O(1) */
